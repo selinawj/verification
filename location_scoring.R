5d256d728880ec7ca2841a2bbe5453ad2725a14a
selinawj <selinawj@msn.com>
2017-10-04 18:27:47 -0400

    added google api locations

--- location_scoring.R
+++ location_scoring.R
@@ -1,5 +1,103 @@
-prepost["score"] <-0 #score starts with 0
-prepost["reason"]<-NA
+##GG PLOTS
+library(ggplot2)
+library(geosphere)
+library(ggmap)
+
+#create new dataframe to store similarity in location
+locationTable = data.frame(prepost$account_id, prepost$latitude, prepost$longitude, prepost$postingLatitude, prepost$postingLongitude)
+locationTable[c("dist", "listLocation", "postLocation", "within")] = ""
+colnames(locationTable) = c("id", "listLat", "listLong", "postLat", "postLong", "dist", "listLocation", "postLocation", "within")
+
+#calculate dist(meters) from list to post
+locationTable$listLat = as.numeric(as.character(locationTable$listLat))
+locationTable$listLong = as.numeric(as.character(locationTable$listLong))
+locationTable$postLat = as.numeric(locationTable$postLat)
+locationTable$postLong = as.numeric(locationTable$postLong)
+row = 1
+listLatCol = 2
+listLongCol = 3
+postLatCol = 4
+postLongCol = 5
+distCol = 6
+listLocationCol = 7
+postLocationCol = 8
+while (row <= nrow(locationTable)){
+  if ((!is.na(locationTable[row,listLongCol])) & (!is.na(locationTable[row,listLatCol]))) {
+    if ((!is.na(locationTable[row,postLatCol])) & (!is.na(locationTable[row,postLongCol]))){
+      locationTable[row, distCol] = distm(c(locationTable[row,listLongCol], locationTable[row,listLatCol]), 
+               c(locationTable[row,postLongCol], locationTable[row,postLatCol]), fun = distHaversine)
+  locationTable[row, listLocationCol] = revgeocode(c(locationTable[row, listLongCol], locationTable[row, listLatCol]))
+  locationTable[row, postLocationCol] = revgeocode(c(locationTable[row, postLongCol], locationTable[row, postLatCol]))
+    } else if ((is.na(locationTable[row,listLongCol])) & (is.na(locationTable[row,listLatCol]))) {
+      if ((is.na(locationTable[row,postLatCol])) & (is.na(locationTable[row,postLongCol]))) {
+        locationTable[row, distCol] = NA
+        locationTable[row, c(listLocationCol, postLocationCol)] = NA
+      }
+    }
+  }
+  row = row + 1
+}
+
+
+row = 1
+listCol = 2
+postCol = 3
+while (row <= nrow(locationTable)){
+  listing = locationTable[row, listCol]
+  posting = locationTable[row, postCol]
+  if ((grepl("", posting)) & (grepl("arverne", listing))) {
+  } else if ((grepl("New York", posting, ignore.case = T)) & (grepl("astoria", listing))) {
+  } else if ((grepl("New York", posting, ignore.case = T)) & (grepl("bronx", listing))) {
+  } else if ((grepl("Brooklyn", posting, ignore.case = T)) & (grepl("brooklyn", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("bushwick", listing))){
+  } else if ((grepl("Jamaica", posting, ignore.case = T)) & (grepl("carnegie hill", listing))){
+  } else if ((grepl("Ozone Park", posting, ignore.case = T)) & (grepl("elmhurst", listing))){
+  } else if ((grepl("Dodgewood", posting, ignore.case = T)) & (grepl("flushing", listing))){
+  } else if ((grepl("Elmhurst", posting, ignore.case = T)) & (grepl("forest hills", listing))){
+  } else if ((grepl("Milford", posting, ignore.case = T)) & (grepl("far rockaway", listing))){
+  } else if ((grepl("Mt. Pleasant", posting, ignore.case = T)) & (grepl("glendale", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("howard beach", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("long island city", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("new rochelle", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("new york", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("ny", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("queens", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("rego park", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("ridgewood", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("rego park", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("springfield gardens", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("staten island", listing))){
+  } else if ((grepl("Flushing", posting, ignore.case = T)) & (grepl("washington heights", listing))){
+    } else if (grepl(posting, listing, ignore.case = T)) {
+  } else if (!grepl(posting, listing, ignore.case = T)){
+    prepost[row,16] = prepost[row,16] - 3.3 #location differs
+  }
+}
+
+#ggplot of total number of each citystate type by descending order
+cityStatePlot = ggplot(cityStateTable, aes(x = reorder(cityStates, -count), y = count)) + geom_bar(stat = "identity")
+cityStatePlot + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#dataframe of how many each posting states are present
+postingStateTable = data.frame(table(prepost$posting_state))
+colnames(postingStateTable) = c('postingStates', 'count')
+
+#ggplot of total number of each postingState type by descending order
+postingStatePlot = ggplot(postingStateTable, aes(x = reorder(postingStates, -count), y = count)) + geom_bar(stat = "identity")
+postingStatePlot + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#dataframe of how many scammers is present in each domain
+domainScammersTable = data.frame(table(prepost$domain, prepost$status))
+colnames(domainScammersTable) = c('domain', 'status', 'count')
+domainScammersPlot = ggplot(domainScammersTable, aes(x = reorder(domain, -count), y = count, fill = status)) + geom_bar(stat = "identity")
+domainScammersPlot + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + labs(title = "Scammers based on Domain", y = "Count", x = "Domain", fill = "Status")
+
+#count how many scammers is present in each domain
+domainScammers = data.frame(table(prepost$domain, prepost$status=='scammer'))
+colnames(domainScammers) = c('domain', 'scammer_status', 'count')
+
+#remove False rows and 0 Count
+domainScammers = domainScammers[domainScammers$scammer_status!="FALSE" & domainScammers$count!="0",]
 
 row = 1
 posting_col = 7 #posting
